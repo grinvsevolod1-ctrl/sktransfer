80701fa9c4ac175e1278c45fcc48bdf180a25734
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-12-05 17:07:14 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -120,13 +120,11 @@ export default function RootLayout({
         <meta name="ICBM" content="53.9045, 27.5615" />
       </head>
       <body className={`${ibmPlexSans.variable} ${bebasNeue.variable} font-sans antialiased`}>
-        <LocaleProvider>
         <ScrollToTop />
         <Header />
         {children}
         <Footer />
         <Analytics />
-      </LocaleProvider>
       </body>
     </html>
   )
